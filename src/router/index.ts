491c866a7cd16d78cb50b93a7798f741c12d164f
Connor Shea <connor.james.shea@gmail.com>
2021-02-07 12:24:37 -0700

    Add a bunch of empty views.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Engines from '../views/Engines.vue'
+import Games from '../views/Games.vue'
+import Genres from '../views/Genres.vue'
+import Platforms from '../views/Platforms.vue'
+import Series from '../views/Series.vue'
+import Stores from '../views/Stores.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +23,36 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/engines',
+    name: 'Engines',
+    component: Engines
+  },
+  {
+    path: '/games',
+    name: 'Games',
+    component: Games
+  },
+  {
+    path: '/genres',
+    name: 'Genres',
+    component: Genres
+  },
+  {
+    path: '/platforms',
+    name: 'Platforms',
+    component: Platforms
+  },
+  {
+    path: '/series',
+    name: 'Series',
+    component: Series
+  },
+  {
+    path: '/stores',
+    name: 'Stores',
+    component: Stores
   }
 ]
 
---
+++ src/views/Stores.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="stores">
+  </div>
+</template>
