7d84a83625c45f483b2440acf9d63d5a7d2f866a
Connor Shea <connor.james.shea@gmail.com>
2021-02-21 10:42:15 -0700

    Remove unnecessary style tag in App.vue.

--- src/App.vue
+++ src/App.vue
@@ -30,13 +30,3 @@ export default Vue.extend({
   }
 });
 </script>
-
-<style lang="scss">
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
-</style>
