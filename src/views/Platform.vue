310f2265faac3c6e37e6a1567422ebca3e8757c0
Connor Shea <connor.james.shea@gmail.com>
2021-02-28 14:00:57 -0700

    List games on Platform page.

--- src/views/Platform.vue
+++ src/views/Platform.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="platform" v-if="data">
-    Platform: {{ data.platform.name }}
+    <h1 class="title">{{ data.platform.name }}</h1>
+
+    <template v-if="data.platform.games.nodes.length > 0">
+      <div class="game-card-list mt-20">
+        <div v-for="game in data.platform.games.nodes" :key="game.id">
+          <GameCard :game="game"/>
+        </div>
+      </div>
+
+      <!-- <%= paginate @games %> -->
+    </template>
+    <template v-else>
+      <p class='has-text-centered mt-50 has-text-muted'>This platform doesn't have any games yet.</p>
+    </template>
   </div>
 </template>
 
@@ -8,9 +21,13 @@
 import { PlatformDocument } from '@/generated/graphql.ts';
 import { defineComponent } from '@vue/composition-api';
 import { useQuery } from 'villus';
+import GameCard from '@/components/GameCard.vue';
 
 export default defineComponent({
   name: 'Platform',
+  components: {
+    GameCard
+  },
   props: {
     id: {
       required: true,
