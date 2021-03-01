3505f712cc6e0f248c83a6919ed549f9c6315447
Connor Shea <connor.james.shea@gmail.com>
2021-02-28 18:47:07 -0700

    Add games to the Engine and Genre views.

--- src/views/Genre.vue
+++ src/views/Genre.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="genre" v-if="data">
-    Genre: {{ data.genre.name }}
+    <h1 class="title">{{ data.genre.name }}</h1>
+
+    <a v-if="data.genre.wikidataId !== null" :href="wikidataUrl">Wikidata</a>
+
+    <template v-if="data.genre.games.nodes.length > 0">
+      <div class="game-card-list mt-20">
+        <div v-for="game in data.genre.games.nodes" :key="game.id">
+          <GameCard :game="game"/>
+        </div>
+      </div>
+
+      <!-- <%= paginate @games %> -->
+    </template>
+    <template v-else>
+      <p class='has-text-centered mt-50 has-text-muted'>This genre doesn't have any games yet.</p>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
 import { GenreDocument } from '@/generated/graphql.ts';
-import { defineComponent } from '@vue/composition-api';
+import { computed, defineComponent } from '@vue/composition-api';
 import { useQuery } from 'villus';
+import GameCard from '@/components/GameCard.vue';
 
 export default defineComponent({
   name: 'Genre',
+  components: {
+    GameCard
+  },
   props: {
     id: {
       required: true,
@@ -25,7 +44,18 @@ export default defineComponent({
       }
     });
 
-    return { data };
+    const wikidataUrl = computed(() => {
+      if (data.value?.genre?.wikidataId === null) {
+        return null;
+      } else {
+        return `https://www.wikidata.org/wiki/Q${data.value?.genre?.wikidataId}`;
+      }
+    });
+
+    return {
+      data,
+      wikidataUrl
+    };
   }
 });
 </script>
