056d49583ee5f5b1c86745887b1a92d2dc14030f
Connor Shea <connor.james.shea@gmail.com>
2021-02-28 20:20:18 -0700

    Misc improvement to router.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,28 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
-import Activity from '../views/Activity.vue';
-import Company from '../views/Company.vue';
-import Companies from '../views/Companies.vue';
-import Engine from '../views/Engine.vue';
-import Engines from '../views/Engines.vue';
-import Games from '../views/Games.vue';
-import Game from '../views/Game.vue';
-import Genre from '../views/Genre.vue';
-import Genres from '../views/Genres.vue';
-import Platform from '../views/Platform.vue';
-import Platforms from '../views/Platforms.vue';
-import Series from '../views/Series.vue';
-import SeriesList from '../views/SeriesList.vue';
-import Store from '../views/Store.vue';
-import Stores from '../views/Stores.vue';
-import Users from '../views/Users.vue';
-import User from '../views/User.vue';
-import UserProfile from '../views/UserProfile.vue';
-import UserActivity from '../views/UserActivity.vue';
-import UserFavorites from '../views/UserFavorites.vue';
-import UserFollowing from '../views/UserFollowing.vue';
-import UserFollowers from '../views/UserFollowers.vue';
+import Home from '@/views/Home.vue';
+import Activity from '@/views/Activity.vue';
+import Company from '@/views/Company.vue';
+import Companies from '@/views/Companies.vue';
+import Engine from '@/views/Engine.vue';
+import Engines from '@/views/Engines.vue';
+import Games from '@/views/Games.vue';
+import Game from '@/views/Game.vue';
+import Genre from '@/views/Genre.vue';
+import Genres from '@/views/Genres.vue';
+import Platform from '@/views/Platform.vue';
+import Platforms from '@/views/Platforms.vue';
+import Series from '@/views/Series.vue';
+import SeriesList from '@/views/SeriesList.vue';
+import Store from '@/views/Store.vue';
+import Stores from '@/views/Stores.vue';
+import Users from '@/views/Users.vue';
+import User from '@/views/User.vue';
+import UserProfile from '@/views/UserProfile.vue';
+import UserActivity from '@/views/UserActivity.vue';
+import UserFavorites from '@/views/UserFavorites.vue';
+import UserFollowing from '@/views/UserFollowing.vue';
+import UserFollowers from '@/views/UserFollowers.vue';
 
 Vue.use(VueRouter);
 
@@ -38,7 +38,7 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
   },
   {
     path: '/activity',
