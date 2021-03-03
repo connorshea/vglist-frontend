dc98cd80dcee66cd43f53cddac29903b34a048c5
Connor Shea <connor.james.shea@gmail.com>
2021-03-02 19:35:33 -0700

    Add basic event list to activity feed page.

---
+++ src/components/EventCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="box">
+    <article class="media">
+      <div class="media-left">
+        <figure class="image is-64x64">
+          <router-link :to="{ name: 'UserProfile', params: { slug: event.user.slug }}">
+            <img v-if="event.user.avatarUrl !== null" :src="event.user.avatarUrl" />
+            <img v-else src="@/assets/images/default-avatar.png"/>
+          </router-link>
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>{{ eventText }}</p>
+
+          <p>
+            <span class="has-text-muted" :title="event.createdAt">
+              <!-- <%= "#{distance_of_time_in_words(event.createdAt, Time.now)} ago" %> -->
+            </span>
+            <!-- <% if policy(event).destroy? %>
+              <%= link_to event_path(event.id),
+                data: { confirm: "Are you sure you want to delete this event?" },
+                method: :delete, class: 'ml-5' do %>
+                <%= svg_icon('trash', css_class: 'has-fill-danger-hoverable', title: 'Delete', options: { style: 'vertical-align: text-top;' }) %>
+              <% end %>
+            <% end %> -->
+          </p>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'EventCard',
+  props: {
+    event: {
+      required: true,
+      type: Object
+    }
+  },
+  setup(props) {
+    const eventText = computed(() => {
+      return 'foo';
+    });
+
+    return {
+      eventText
+    };
+  }
+});
+</script>
--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,5 +1,5 @@
 <template>  
-  <div class="ml-50 mr-50 mr-0-mobile ml-0-mobile">
+  <div class="ml-50 mr-50 mr-0-mobile ml-0-mobile" v-if="data">
     <h1 class="title">Global Activity</h1>
 
     <!--
@@ -15,14 +15,38 @@
         </ul>
       </div>
     <% end %>
-
-    <% @events.each do |event| %>
-      <% if handleable_event?(event) %>
-        <%= render 'shared/event', event: event %>
-      <% end %>
-    <% end %>
     -->
+
+    <template v-for="event in data.activity.nodes">
+      <!-- <template v-if="eventIsHandleable"> -->
+        <EventCard :event="event" :key="event.id"></EventCard>
+      <!-- </template> -->
+    </template>
 
     <!-- <%= paginate @events %> -->
   </div>
 </template>
+
+<script lang="ts">
+import { ActivityFeed, ActivityFeedDocument } from '@/generated/graphql.ts';
+import { defineComponent } from '@vue/composition-api';
+import { useQuery } from 'villus';
+import EventCard from '@/components/EventCard.vue';
+
+export default defineComponent({
+  name: 'Activity',
+  components: {
+    EventCard
+  },
+  setup() {
+    const { data } = useQuery({
+      query: ActivityFeedDocument,
+      variables: {
+        feedType: ActivityFeed.Global
+      }
+    });
+
+    return { data };
+  }
+});
+</script>
