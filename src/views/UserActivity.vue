fce2b2e739be87fba103c0b4ee206ad879292942
Connor Shea <connor.james.shea@gmail.com>
2021-03-03 17:54:19 -0700

    Add events to activity tab on user page.

--- src/views/UserActivity.vue
+++ src/views/UserActivity.vue
@@ -1,23 +1,48 @@
 <template>
-  <div class="activity">
-    <!-- TODO -->
-    <p>Activity</p>
+  <div class="ml-50 mr-50 mr-0-mobile ml-0-mobile" v-if="data">
+    <template v-if="data.user.activity.nodes.length > 0">
+      <template v-for="event in data.user.activity.nodes">
+        <EventCard :event="event" :key="event.id"></EventCard>
+      </template>
+    </template>
+    <template v-else>
+      <!-- This technically shouldn't be possible since there's an event for a
+          user's creation, but since users can delete events we include a
+          message, just in case. -->
+      <p class="has-text-centered has-text-muted">No activity for this user yet.</p>
+    </template>
+
+    <!-- <%= paginate @events %> -->
   </div>
 </template>
 
 <script lang="ts">
+import { UserActivityDocument } from '@/generated/graphql';
 import { defineComponent } from '@vue/composition-api';
+import { useQuery } from 'villus';
+import EventCard from '@/components/EventCard.vue';
 
 export default defineComponent({
   name: 'UserActivity',
+  components: {
+    EventCard
+  },
   props: {
     user: {
       required: true,
       type: Object
     }
   },
-  setup() {
-    // TODO
+  setup(props) {
+    const { data } = useQuery({
+      query: UserActivityDocument,
+      variables: {
+        userId: props.user.id,
+        cursor: ''
+      }
+    });
+
+    return { data };
   }
 });
 </script>
