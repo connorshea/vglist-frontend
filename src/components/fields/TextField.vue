6bae2fa196d895d8ce955a9ff4954db623a5ad2f
Connor Shea <connor.james.shea@gmail.com>
2021-03-12 22:36:17 -0700

    Add a few field components.
    And convert them to use the composition API.

---
+++ src/components/fields/TextField.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="field">
+    <label class="label" v-bind:for="textFieldId">{{ label }}</label>
+    <div class="control">
+      <input
+        autocomplete="off"
+        class="input"
+        type="text"
+        :required="required"
+        v-bind:name="textFieldName"
+        v-bind:id="textFieldId"
+        v-bind:value="dataValue"
+        v-on:input="$emit('input', $event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'TextField',
+  props: {
+    formClass: {
+      type: String,
+      required: true
+    },
+    attribute: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    // Optional to allow for null.
+    value: {
+      type: String,
+      required: false
+    },
+    required: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  setup(props) {
+    const dataValue = ref(props.value);
+
+    const textFieldName = computed(() => `${props.formClass}[${props.attribute}]`);
+    const textFieldId = computed(() => `${props.formClass}_${props.attribute}`);
+
+    return {
+      textFieldName,
+      textFieldId,
+      dataValue
+    }
+  }
+});
+</script>
