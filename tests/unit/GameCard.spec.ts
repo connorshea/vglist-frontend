130659c82ed48db254c13d5bcbef8f324ea98d5a
Connor Shea <connor.james.shea@gmail.com>
2021-03-15 19:40:47 -0600

    Simplify router mocking.

--- tests/unit/GameCard.spec.ts
+++ tests/unit/GameCard.spec.ts
@@ -1,17 +1,14 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
-import VueRouter from 'vue-router'
+import { createLocalVue, mount } from '@vue/test-utils'
 import GameCard from '@/components/GameCard.vue'
 
 const localVue = createLocalVue();
-localVue.use(VueRouter);
-const router = new VueRouter();
 
 describe('GameCard.vue', () => {
   // Disabled for now because I can't figure out how to get Villus to work in tests.
   xit('renders platforms and developers when passed', () => {
-    const wrapper = shallowMount(GameCard, {
+    const wrapper = mount(GameCard, {
+      stubs: ['router-link', 'router-view'],
       localVue,
-      router,
       propsData: {
         game: {
           id: 1,
