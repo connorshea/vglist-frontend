6c5a7096dd452ba5383f1b6510064e6f0b6d4af7
Connor Shea <connor.james.shea@gmail.com>
2021-03-23 18:38:41 -0600

    Add users sorting to users index.
    It works!

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -2,23 +2,27 @@
   <div class="users" v-if="data">
     <h1 class="title">Users</h1>
 
-    <div class="dropdown is-fullwidth mr-0-mobile">
-      <div class="dropdown-trigger is-fullwidth-mobile">
+    <div class="dropdown is-fullwidth mr-0-mobile" :class="{ 'is-active': dropdownIsActive }">
+      <div class="dropdown-trigger is-fullwidth-mobile" @click="toggleDropdown">
         <button class="button is-fullwidth" aria-haspopup="true" aria-controls="dropdown-menu">
           <span>
-            <!-- <%= params[:order_by].nil? ? "Sort" : "Sort by #{params[:order_by].titleize.downcase}" %> -->
+            {{ sortBy === null ? "Sort" : `Sort by ${sortBy.toLowerCase().replace('_', ' ')}` }}
           </span>
-          <span class="icon">
-            <!-- <%= svg_icon('chevron-down', height: 15, aria: false) %> -->
-          </span>
+          <SvgIcon :name="'chevron-down'" :size="15" :classes="['icon']"/>
         </button>
       </div>
 
       <div class="dropdown-menu is-fullwidth" id="sort-dropdown-menu" role="menu">
         <div class="dropdown-content">
-          <!-- <%= sort_dropdown_link(nil, 'Default') %> -->
-          <!-- <%= sort_dropdown_link(:most_followers, 'Most Followers') %> -->
-          <!-- <%= sort_dropdown_link(:most_games, 'Most Games') %> -->
+          <template v-for="sortOption in sortOptions">
+            <a :key="sortOption.name"
+               class="dropdown-item"
+               :class="{ 'has-text-weight-bold': sortBy === sortOption.value }"
+               @click="setSortBy(sortOption.value)"
+            >
+              {{ sortOption.name }}
+            </a>
+          </template>
         </div>
       </div>
     </div>
@@ -35,24 +39,58 @@
 
 <script lang="ts">
 import { UsersDocument } from '@/generated/graphql';
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, Ref, ref } from '@vue/composition-api';
 import { useQuery } from 'villus';
 import UserCard from '@/components/UserCard.vue';
+import SvgIcon from '@/components/SvgIcon.vue';
 
 export default defineComponent({
   name: 'Users',
   components: {
-    UserCard
+    UserCard,
+    SvgIcon
   },
   setup() {
-    const { data } = useQuery({
+    const sortBy: Ref<sortOptionsType> = ref(null);
+    const queryVariables = ref({ cursor: '', sortBy: sortBy });
+    const { execute, data } = useQuery({
       query: UsersDocument,
-      variables: {
-        cursor: ''
-      }
+      variables: queryVariables
     });
 
-    return { data };
+    type sortOptionsType = null | 'MOST_FOLLOWERS' | 'MOST_GAMES';
+
+    const setSortBy = (sort: sortOptionsType) => {
+      sortBy.value = sort;
+      execute();
+    };
+
+    const sortOptions: Array<{ name: string, value: sortOptionsType }> = [
+      {
+        name: 'Default',
+        value: null
+      },
+      {
+        name: 'Most Followers',
+        value: 'MOST_FOLLOWERS'
+      },
+      {
+        name: 'Most Games',
+        value: 'MOST_GAMES'
+      }
+    ];
+
+    const dropdownIsActive = ref(false);
+    const toggleDropdown = () => dropdownIsActive.value = !dropdownIsActive.value;
+
+    return {
+      data,
+      sortBy,
+      setSortBy,
+      sortOptions,
+      dropdownIsActive,
+      toggleDropdown
+    };
   }
 });
 </script>
