c5f5354421bec5fe0038b7506f1969bd396c5b2c
Connor Shea <connor.james.shea@gmail.com>
2021-05-02 12:44:16 -0600

    Add statistics data in cards to Admin Dashboard.
    Doesn't actually use live data for now, and the solution for getting
    the specific pieces of data we want is kind of gross.
    
    Need to fix the spacing on the keys used for level headings.

--- src/views/admin/AdminDashboard.vue
+++ src/views/admin/AdminDashboard.vue
@@ -1,6 +1,50 @@
 <template>
   <div v-if="data">
-    <!-- TODO: Statistics tables. -->
+    <div class="card pt-15 pb-15">
+      <div class="has-text-centered has-text-weight-bold mb-25">Statistics</div>
+      <!-- Breaks the counts into two separate levels. -->
+      <nav class="level">
+        <div class="level-item has-text-centered" v-for="(count, key) in recordCountRows[0]" :key="key">
+          <div>
+            <p class="heading">{{ key }}</p>
+            <p class="title">{{ count }}</p>
+          </div>
+        </div>
+      </nav>
+
+      <nav class="level">
+        <div class="level-item has-text-centered" v-for="(count, key) in recordCountRows[1]" :key="key">
+          <div>
+            <p class="heading">{{ key }}</p>
+            <p class="title">{{ count }}</p>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="card mt-30 pt-15 pb-15">
+      <div class="has-text-centered has-text-weight-bold mb-25">External Identifiers</div>
+      <nav class="level">
+        <div class="level-item has-text-centered" v-for="(count, key) in externalIdCounts" :key="key">
+          <div>
+            <p class="heading">{{ key }}</p>
+            <p class="title">{{ count }}</p>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="card mt-30 pt-15 pb-15">
+      <div class="has-text-centered has-text-weight-bold mb-25">Version Counts</div>
+      <nav class="level">
+        <div class="level-item has-text-centered" v-for="(count, key) in versionCounts" :key="key">
+          <div>
+            <p class="heading">{{ key }}</p>
+            <p class="title">{{ count }}</p>
+          </div>
+        </div>
+      </nav>
+    </div>
 
     <h3 class="has-text-centered mt-25 mb-10 has-text-weight-bold">Statistics Over Time</h3>
     <div class="table-container">
@@ -112,7 +156,7 @@
 
 <script lang="ts">
 import { useQuery } from 'villus';
-import { defineComponent } from '@vue/composition-api';
+import { computed, defineComponent } from '@vue/composition-api';
 import { SiteStatisticsDocument } from '@/generated/graphql';
 
 export default defineComponent({
@@ -139,9 +183,77 @@ export default defineComponent({
       return new Date(timestamp).toLocaleString('en-US', options);
     };
 
+    // TODO: Replace with actual GraphQL query once it exists.
+    const liveStatistics = {
+      users: 123,
+      games: 123,
+      platforms: 123,
+      series: 123,
+      engines: 123,
+      companies: 123,
+      genres: 123,
+      stores: 123,
+      events: 123,
+      gamePurchases: 123,
+      relationships: 123,
+      gamesWithCovers: 123,
+      gamesWithReleaseDates: 123,
+      bannedUsers: 123,
+      mobygamesIds: 123,
+      pcgamingwikiIds: 123,
+      wikidataIds: 123,
+      giantbombIds: 123,
+      steamAppIds: 123,
+      epicGamesStoreIds: 123,
+      gogIds: 123,
+      igdbIds: 123,
+      companyVersions: 123,
+      gameVersions: 123,
+      genreVersions: 123,
+      engineVersions: 123,
+      platformVersions: 123,
+      seriesVersions: 123
+    };
+
+    // Will return a pair of arrays of record counts, one for each row of
+    // statistics to display.
+    const recordCountRows = computed(() => {
+      let recordKeys = ['users', 'games', 'platforms', 'series', 'engines', 'companies', 'genres', 'stores', 'events', 'gamePurchases', 'relationships', 'gamesWithCovers', 'gamesWithReleaseDates', 'bannedUsers'];
+
+      // Get half of the record keys array's length so we can split it in half.
+      let half = Math.ceil(recordKeys.length / 2);
+
+      let recordPairs = Object.entries(liveStatistics).filter(([key]) => recordKeys.includes(key));
+
+      // Cursed solution abusing splice to split the object in half.
+      return [
+        Object.fromEntries(recordPairs.splice(0, half)),
+        Object.fromEntries(recordPairs.splice(-half))
+      ];
+    });
+
+    const externalIdCounts = computed(() => {
+      let externalIdKeys = ['mobygamesIds', 'pcgamingwikiIds', 'wikidataIds', 'giantbombIds', 'steamAppIds', 'epicGamesStoreIds', 'gogIds', 'igdbIds'];
+
+      return Object.fromEntries(
+        Object.entries(liveStatistics).filter(([key]) => externalIdKeys.includes(key))
+      );
+    });
+
+    const versionCounts = computed(() => {
+      let versionKeys = ['companyVersions', 'gameVersions', 'genreVersions', 'engineVersions', 'platformVersions', 'seriesVersions'];
+
+      return Object.fromEntries(
+        Object.entries(liveStatistics).filter(([key]) => versionKeys.includes(key))
+      );
+    });
+
     return {
       data,
-      formatTimestamp
+      formatTimestamp,
+      recordCountRows,
+      externalIdCounts,
+      versionCounts
     };
   }
 });
