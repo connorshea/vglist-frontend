9309100fba35a72813b8c6838e9858b3b6bd22a1
Connor Shea <connor.james.shea@gmail.com>
2021-05-22 19:04:08 -0600

    Fill out game form and get it partially working.
    Still a decent number of problems left to resolve, need to work on
    solving the remaining issues.

--- src/components/forms/GameForm.vue
+++ src/components/forms/GameForm.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="title">{{ title }}</h1>
 
-    <TextField
+    <text-field
       :form-class="formData.class"
       :attribute="formData.name.attribute"
       :label="formData.name.label"
@@ -10,13 +10,111 @@
       v-model="game.name"
     />
 
-    <NumberField
+    <date-field
+      :form-class="formData.class"
+      :attribute="formData.releaseDate.attribute"
+      :label="formData.releaseDate.label"
+      :required="false"
+      v-model="game.releaseDate"
+    />
+
+    <multi-select
+      :label="formData.genres.label"
+      v-model="game.genres"
+      :search-path="'genreSearch'"
+      :graphqlQuery="GenreSearchDocument"
+    />
+
+    <multi-select
+      :label="formData.engines.label"
+      v-model="game.engines"
+      :search-path="'engineSearch'"
+      :graphqlQuery="EngineSearchDocument"
+    />
+
+    <multi-select
+      :label="formData.developers.label"
+      v-model="game.developers"
+      :search-path="'companySearch'"
+      :graphqlQuery="CompanySearchDocument"
+    />
+
+    <multi-select
+      :label="formData.publishers.label"
+      v-model="game.publishers"
+      :search-path="'companySearch'"
+      :graphqlQuery="CompanySearchDocument"
+    />
+
+    <multi-select
+      :label="formData.platforms.label"
+      v-model="game.platforms"
+      :search-path="'platformSearch'"
+      :graphqlQuery="PlatformSearchDocument"
+    />
+
+    <single-select
+      :label="formData.series.label"
+      v-model="game.series"
+      :search-path="'seriesSearch'"
+      :graphqlQuery="SeriesSearchDocument"
+    />
+
+    <number-field
       :form-class="formData.class"
       :attribute="formData.wikidataId.attribute"
       :label="formData.wikidataId.label"
-      :required="false"
       v-model="game.wikidataId"
-      :min="1"
+    />
+
+    <multi-select-generic
+      :form-class="formData.class"
+      :attribute="formData.steamAppIds.attribute"
+      :label="formData.steamAppIds.label"
+      :v-select-label="'app_id'"
+      v-model="game.steamAppIds"
+    />
+
+    <text-field
+      :form-class="formData.class"
+      :attribute="formData.epicGamesStoreId.attribute"
+      :label="formData.epicGamesStoreId.label"
+      v-model="game.epicGamesStoreId"
+    />
+
+    <text-field
+      :form-class="formData.class"
+      :attribute="formData.gogId.attribute"
+      :label="formData.gogId.label"
+      v-model="game.gogId"
+    />
+
+    <text-field
+      :form-class="formData.class"
+      :attribute="formData.igdbId.attribute"
+      :label="formData.igdbId.label"
+      v-model="game.igdbId"
+    />
+
+    <text-field
+      :form-class="formData.class"
+      :attribute="formData.pcgamingwikiId.attribute"
+      :label="formData.pcgamingwikiId.label"
+      v-model="game.pcgamingwikiId"
+    />
+
+    <text-field
+      :form-class="formData.class"
+      :attribute="formData.mobygamesId.attribute"
+      :label="formData.mobygamesId.label"
+      v-model="game.mobygamesId"
+    />
+
+    <text-field
+      :form-class="formData.class"
+      :attribute="formData.giantbombId.attribute"
+      :label="formData.giantbombId.label"
+      v-model="game.giantbombId"
     />
 
     <button
@@ -32,17 +130,25 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, Ref, ref } from '@vue/composition-api';
+import { computed, defineComponent, ref } from '@vue/composition-api';
 import TextField from '@/components/fields/TextField.vue';
+import SingleSelect from '@/components/fields/SingleSelect.vue';
 import NumberField from '@/components/fields/NumberField.vue';
+import MultiSelect from '@/components/fields/MultiSelect.vue';
+import MultiSelectGeneric from '@/components/fields/MultiSelectGeneric.vue';
+import DateField from '@/components/fields/DateField.vue';
 import { useMutation } from 'villus';
-import { UpdateGameDocument, CreateGameDocument } from '@/generated/graphql';
+import { UpdateGameDocument, CreateGameDocument, CompanySearchDocument, GenreSearchDocument, EngineSearchDocument, PlatformSearchDocument, SeriesSearchDocument } from '@/generated/graphql';
 
 export default defineComponent({
   name: 'GameForm',
   components: {
     TextField,
-    NumberField
+    SingleSelect,
+    NumberField,
+    MultiSelect,
+    MultiSelectGeneric,
+    DateField
   },
   props: {
     id: {
@@ -55,10 +161,74 @@ export default defineComponent({
       type: String,
       default: ''
     },
-    wikidataId: {
+    releaseDate: {
+      type: Date,
+      required: false
+    },
+    genres: {
+      type: Array,
       required: false,
+      default: () => []
+    },
+    engines: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+    developers: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+    publishers: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+    platforms: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+    series: {
+      type: Object,
+      required: false,
+      default: () => {
+        return { name: '' };
+      }
+    },
+    steamAppIds: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+    epicGamesStoreId: {
       type: String,
-      default: null
+      required: false
+    },
+    gogId: {
+      type: String,
+      required: false
+    },
+    igdbId: {
+      type: String,
+      required: false
+    },
+    wikidataId: {
+      type: Number,
+      required: false
+    },
+    pcgamingwikiId: {
+      type: String,
+      required: false
+    },
+    mobygamesId: {
+      type: String,
+      required: false
+    },
+    giantbombId: {
+      type: String,
+      required: false
     },
     // Should be either 'create' or 'update'
     formState: {
@@ -69,20 +239,86 @@ export default defineComponent({
   setup(props, context) {
     const formData = {
       class: 'game',
+      cover: {
+        label: 'Cover'
+      },
       name: {
-        label: 'Game name',
+        label: 'Game title',
         attribute: 'name'
+      },
+      releaseDate: {
+        label: 'Release Date',
+        attribute: 'release_date'
+      },
+      genres: {
+        label: 'Genres'
+      },
+      engines: {
+        label: 'Engines'
+      },
+      developers: {
+        label: 'Developers'
+      },
+      publishers: {
+        label: 'Publishers'
+      },
+      platforms: {
+        label: 'Platforms'
+      },
+      series: {
+        label: 'Series'
+      },
+      steamAppIds: {
+        label: 'Steam Application IDs',
+        attribute: 'steam_app_ids'
+      },
+      epicGamesStoreId: {
+        label: 'Epic Games Store ID',
+        attribute: 'epic_games_store_id'
+      },
+      gogId: {
+        label: 'GOG.com ID',
+        attribute: 'gog_id'
+      },
+      igdbId: {
+        label: 'IGDB ID',
+        attribute: 'igdb_id'
       },
       wikidataId: {
         label: 'Wikidata ID',
         attribute: 'wikidata_id'
+      },
+      pcgamingwikiId: {
+        label: 'PCGamingWiki ID',
+        attribute: 'pcgamingwiki_id'
+      },
+      mobygamesId: {
+        label: 'MobyGames ID',
+        attribute: 'mobygames_id'
+      },
+      giantbombId: {
+        label: 'Giant Bomb ID',
+        attribute: 'giantbomb_id'
       }
-    }
+    };
 
-    const game: Ref<{ id: string | null, name: string | null, wikidataId: string | null }> = ref({
+    const game = ref({
       id: props.id ?? null, 
       name: props.name ?? '',
-      wikidataId: props.wikidataId ?? ''
+      wikidataId: props.wikidataId ?? '',
+      series: props.series as { id: string, name: string } ,
+      platforms: props.platforms as Array<{ id: string, name: string }>,
+      engines: props.engines as Array<{ id: string, name: string }>,
+      genres: props.genres as Array<{ id: string, name: string }>,
+      developers: props.developers as Array<{ id: string, name: string }>,
+      publishers: props.publishers as Array<{ id: string, name: string }>,
+      steamAppIds: props.steamAppIds as Array<number>,
+      epicGamesStoreId: props.epicGamesStoreId,
+      gogId: props.gogId,
+      igdbId: props.igdbId,
+      pcgamingwikiId: props.pcgamingwikiId,
+      mobygamesId: props.mobygamesId,
+      giantbombId: props.giantbombId
     });
 
     const title = computed(() => {
@@ -99,8 +335,20 @@ export default defineComponent({
 
     const { data: createGameData, execute: executeCreateGame, error: createGameErrors } = useMutation(CreateGameDocument);
     let createGame = () => {
-      let { name, wikidataId } = game.value;
-      executeCreateGame({ name: name ?? '', wikidataId }).then(() => {
+      let { name, wikidataId, developers, publishers, engines, genres, platforms, series, steamAppIds, ...gameValues } = game.value;
+      let executeVariables = {
+        name: name ?? '',
+        wikidataId: wikidataId.toString(),
+        seriesId: series?.id,
+        genreIds: genres.map(genre => genre.id),
+        engineIds: engines.map(engine => engine.id),
+        developerIds: developers.map(developer => developer.id),
+        publisherIds: publishers.map(publisher => publisher.id),
+        platformIds: platforms.map(platform => platform.id),
+        steamAppIds: steamAppIds.map(appId => Number(appId)),
+        ...gameValues
+      };
+      executeCreateGame(executeVariables).then(() => {
         if (createGameData.value.createGame?.game?.id) {
           context.root.$router.push({ name: 'Game', params: { id: createGameData.value.createGame.game.id }});
         } else {
@@ -112,9 +360,21 @@ export default defineComponent({
 
     const { data: updateGameData, execute: executeUpdateGame, error: updateGameErrors } = useMutation(UpdateGameDocument);
     let updateGame = () => {
-      let { id: gameId, ...gameValues } = game.value;
+      let { id: gameId, wikidataId, developers, publishers, engines, genres, platforms, series, steamAppIds, ...gameValues } = game.value;
       if (gameId === null) { throw Error('Something went wrong and id is null.') }
-      executeUpdateGame({ gameId: gameId, ...gameValues }).then(() => {
+      let executeVariables = {
+        gameId: gameId,
+        wikidataId: wikidataId.toString(),
+        seriesId: series?.id,
+        genreIds: genres.map(genre => genre.id),
+        engineIds: engines.map(engine => engine.id),
+        developerIds: developers.map(developer => developer.id),
+        publisherIds: publishers.map(publisher => publisher.id),
+        platformIds: platforms.map(platform => platform.id),
+        steamAppIds: steamAppIds.map(appId => Number(appId)),
+        ...gameValues
+      };
+      executeUpdateGame(executeVariables).then(() => {
         if (updateGameData.value.updateGame?.game?.id) {
           context.root.$router.push({ name: 'Game', params: { id: props.id }});
         } else {
@@ -131,7 +391,12 @@ export default defineComponent({
       onSubmit,
       createGame,
       updateGame,
-      cancelRouterLink
+      cancelRouterLink,
+      CompanySearchDocument,
+      EngineSearchDocument,
+      GenreSearchDocument,
+      PlatformSearchDocument,
+      SeriesSearchDocument
     };
   }
 });
--- src/views/games/EditGame.vue
+++ src/views/games/EditGame.vue
@@ -35,22 +35,84 @@ export default defineComponent({
       variables: {
         id: props.id
       },
-      fetchOnMount: false
+      fetchOnMount: false,
+      cachePolicy: 'network-only'
     });
 
-    const game: Ref<{ id: string, name: string | null, wikidataId: number | string | null }> = ref({
+    type GameRef = Ref<{
+      id: string,
+      name: string | null,
+      wikidataId: number | string | null,
+      releaseDate: Date | null,
+      series: { id: string, name: string } | null,
+      platforms: Array<{ id: string, name: string}>,
+      engines: Array<{ id: string, name: string}>,
+      genres: Array<{ id: string, name: string}>,
+      developers: Array<{ id: string, name: string}>,
+      publishers: Array<{ id: string, name: string}>,
+      steamAppIds: Array<number>,
+      epicGamesStoreId: string | null,
+      gogId: string | null,
+      igdbId: string | null,
+      pcgamingwikiId: string | null,
+      mobygamesId: string | null,
+      giantbombId: string | null
+    }>;
+
+    const game: GameRef = ref({
       id: props.id,
       name: null,
-      wikidataId: null
+      wikidataId: null,
+      releaseDate: null,
+      series: null,
+      platforms: [],
+      engines: [],
+      genres: [],
+      developers: [],
+      publishers: [],
+      steamAppIds: [],
+      epicGamesStoreId: null,
+      gogId: null,
+      igdbId: null,
+      pcgamingwikiId: null,
+      mobygamesId: null,
+      giantbombId: null
     });
 
     // Don't try to render the form until the data is fully loaded, to make
     // sure we can pre-populate the form as necessary.
     const isLoaded = ref(false);
 
+    type VglistRecord = { id: string, name: string };
+
     executeQuery().then(({ data }) => {
       game.value.name = data?.game?.name ?? null;
       game.value.wikidataId = data?.game?.wikidataId ?? null;
+      game.value.releaseDate = new Date(data?.game?.releaseDate) ?? null;
+      game.value.steamAppIds = data?.game?.steamAppIds ?? [];
+      game.value.series = data?.game?.series === null ? null : { id: data?.game?.series?.id, name: data?.game?.series?.name } as VglistRecord;
+      game.value.engines = data?.game?.engines?.nodes?.map(engine => {
+        return { id: engine?.id, name: engine?.name } as VglistRecord;
+      }) ?? [];
+      game.value.genres = data?.game?.genres?.nodes?.map(genre => {
+        return { id: genre?.id, name: genre?.name } as VglistRecord;
+      }) ?? [];
+      game.value.developers = data?.game?.developers?.nodes?.map(developer => {
+        return { id: developer?.id, name: developer?.name } as VglistRecord;
+      }) ?? [];
+      game.value.publishers = data?.game?.publishers?.nodes?.map(publisher => {
+        return { id: publisher?.id, name: publisher?.name } as VglistRecord;
+      }) ?? [];
+      game.value.platforms = data?.game?.platforms?.nodes?.map(platform => {
+        return { id: platform?.id, name: platform?.name } as VglistRecord;
+      }) ?? [];
+      game.value.steamAppIds = data?.game?.steamAppIds ?? [];
+      game.value.epicGamesStoreId = data?.game?.epicGamesStoreId ?? null;
+      game.value.gogId = data?.game?.gogId ?? null;
+      game.value.igdbId = data?.game?.igdbId ?? null;
+      game.value.pcgamingwikiId = data?.game?.pcgamingwikiId ?? null;
+      game.value.mobygamesId = data?.game?.mobygamesId ?? null;
+      game.value.giantbombId = data?.game?.giantbombId ?? null;
       isLoaded.value = true;
     });
 
