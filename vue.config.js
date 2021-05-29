07655dc8aae26fe58bacb45d15f6b39c7428d4f3
Connor Shea <connor.james.shea@gmail.com>
2021-05-29 12:06:50 -0600

    Set the publicPath differently in production.

--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,7 @@ module.exports = {
       .use('html-loader')
       .loader('html-loader')
       .end();
-  }
+  },
+  // For properly setting the base path in 'production' with GitLab Pages.
+  publicPath: process.env.NODE_ENV === 'production' ? '/' + process.env.CI_PROJECT_NAME + '/' : '/'
 }
