68e235f699247989a0075fdf75cd282133bfbbad
Connor Shea <connor.james.shea@gmail.com>
2021-06-05 10:42:53 -0600

    Add platform queries to mocks.

--- tests/mocks/graphql-handlers.ts
+++ tests/mocks/graphql-handlers.ts
@@ -1,8 +1,16 @@
 import {
+  DeletePlatformMutation,
+  DeletePlatformMutationVariables,
   DeleteSeriesMutation,
   DeleteSeriesMutationVariables,
   FavoriteGameMutation,
   FavoriteGameMutationVariables,
+  PlatformQuery,
+  PlatformQueryVariables,
+  PlatformSearchQuery,
+  PlatformSearchQueryVariables,
+  PlatformsQuery,
+  PlatformsQueryVariables,
   SeriesListQuery,
   SeriesListQueryVariables,
   SeriesQuery,
@@ -158,6 +166,120 @@ export const handlers = [
       return res(
         ctx.data({
           deleteSeries: {
+            deleted: true
+          }
+        })
+      );
+    }
+  ),
+
+  graphql.query<PlatformsQuery, PlatformsQueryVariables>(
+    'Platforms',
+    (req, res, ctx) => {
+      return res(
+        ctx.data({
+          platforms: {
+            nodes: [
+              {
+                id: '1',
+                name: 'Nintendo Switch'
+              },
+              {
+                id: '2',
+                name: 'Nintendo DS'
+              }
+            ],
+            pageInfo: {
+              hasPreviousPage: false,
+              hasNextPage: false,
+              startCursor: 'mA',
+              endCursor: 'mB'
+            }
+          }
+        })
+      );
+    }
+  ),
+
+  graphql.query<PlatformQuery, PlatformQueryVariables>(
+    'Platform',
+    (req, res, ctx) => {
+      const { id } = req.variables;
+
+      return res(
+        ctx.data({
+          platform: {
+            id: id,
+            name: 'Windows',
+            wikidataId: 123,
+            games: {
+              nodes: [
+                {
+                  id: '1',
+                  name: 'Half-Life 1',
+                  coverUrl: null,
+                  platforms: {
+                    nodes: [{ name: 'Windows' }]
+                  },
+                  developers: {
+                    nodes: [{ name: 'Valve' }]
+                  },
+                  isFavorited: false
+                },
+                {
+                  id: '2',
+                  name: 'Half-Life 2',
+                  coverUrl: null,
+                  platforms: {
+                    nodes: [{ name: 'Windows' }]
+                  },
+                  developers: {
+                    nodes: [{ name: 'Valve' }]
+                  },
+                  isFavorited: false
+                }
+              ],
+              pageInfo: {
+                hasPreviousPage: false,
+                hasNextPage: false,
+                startCursor: 'mA',
+                endCursor: 'mB'
+              }
+            }
+          }
+        })
+      );
+    }
+  ),
+
+  graphql.query<PlatformSearchQuery, PlatformSearchQueryVariables>(
+    'PlatformSearch',
+    (req, res, ctx) => {
+      return res(
+        ctx.data({
+          platformSearch: {
+            nodes: [
+              {
+                id: '1',
+                name: 'Nintendo Switch'
+              },
+              {
+                id: '2',
+                name: 'Nintendo Wii'
+              }
+            ]
+          }
+        })
+      );
+    }
+  ),
+
+  graphql.mutation<DeletePlatformMutation, DeletePlatformMutationVariables>(
+    'DeletePlatform',
+    (req, res, ctx) => {
+      return res(
+        ctx.data({
+          deletePlatform: {
             deleted: true
           }
         })
