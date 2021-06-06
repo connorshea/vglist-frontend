52cf86c0ec52dbc31f2373ddcd443d77d3a07772
Connor Shea <connor.james.shea@gmail.com>
2021-06-06 14:21:16 -0600

    Add basic tests for CreateEngine and EditEngine views.

---
+++ tests/unit/views/engines/CreateEngine.spec.ts
@@ -0,0 +1,42 @@
+import { mount } from '@vue/test-utils';
+import CreateEngine from '@/views/engines/CreateEngine.vue';
+import { createStore } from 'vuex';
+import { createClient, VILLUS_CLIENT } from 'villus';
+import { createMemoryHistory, createRouter } from 'vue-router';
+import { routes } from '@/router';
+import waitForExpect from 'wait-for-expect';
+
+describe('CreateEngine.vue', () => {
+  it('renders engine form', async () => {
+    const store = createStore({
+      state() {
+        return { userSignedIn: true };
+      }
+    });
+
+    const router = createRouter({ 
+      history: createMemoryHistory(),
+      routes
+    });
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(CreateEngine, {
+      global: {
+        plugins: [
+          store,
+          router
+        ],
+        provide: {
+          [VILLUS_CLIENT as symbol]: createClient({
+            url: 'http://test/graphql',
+          }),
+        }
+      }
+    });
+
+    await waitForExpect(() => {
+      expect(wrapper.get('[data-test-id="form-title"').text()).toMatch('Creating a new engine');
+    });
+  });
+});
---
+++ tests/unit/views/engines/EditEngine.spec.ts
@@ -0,0 +1,45 @@
+import { mount } from '@vue/test-utils';
+import EditEngine from '@/views/engines/EditEngine.vue';
+import { createStore } from 'vuex';
+import { createClient, VILLUS_CLIENT } from 'villus';
+import { createMemoryHistory, createRouter } from 'vue-router';
+import { routes } from '@/router';
+import waitForExpect from 'wait-for-expect';
+
+describe('EditEngine.vue', () => {
+  it('renders engine form', async () => {
+    const store = createStore({
+      state() {
+        return { userSignedIn: true };
+      }
+    });
+
+    const router = createRouter({ 
+      history: createMemoryHistory(),
+      routes
+    });
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(EditEngine, {
+      global: {
+        plugins: [
+          store,
+          router
+        ],
+        provide: {
+          [VILLUS_CLIENT as symbol]: createClient({
+            url: 'http://test/graphql',
+          }),
+        }
+      },
+      props: {
+        id: '1'
+      }
+    });
+
+    await waitForExpect(() => {
+      expect(wrapper.get('[data-test-id="form-title"').text()).toMatch('Editing Source Engine');
+    });
+  });
+});
