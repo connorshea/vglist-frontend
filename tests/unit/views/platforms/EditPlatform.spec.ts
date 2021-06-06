313c734a3bbab12834285237a8a8a27cc1adbd7a
Connor Shea <connor.james.shea@gmail.com>
2021-06-06 14:17:25 -0600

    Add basic tests for CreatePlatform and EditPlatform views.

---
+++ tests/unit/views/platforms/EditPlatform.spec.ts
@@ -0,0 +1,46 @@
+import { mount } from '@vue/test-utils';
+import EditPlatform from '@/views/platforms/EditPlatform.vue';
+import { createStore } from 'vuex';
+import { createClient, VILLUS_CLIENT } from 'villus';
+import { createMemoryHistory, createRouter } from 'vue-router';
+import { routes } from '@/router';
+import waitForExpect from 'wait-for-expect';
+
+describe('EditPlatform.vue', () => {
+  it('renders platform form', async () => {
+    const store = createStore({
+      state() {
+        return { userSignedIn: true };
+      }
+    });
+
+    const router = createRouter({ 
+      history: createMemoryHistory(),
+      routes
+    });
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(EditPlatform, {
+      global: {
+        plugins: [
+          store,
+          router
+        ],
+        stubs: ['SvgIcon'],
+        provide: {
+          [VILLUS_CLIENT as symbol]: createClient({
+            url: 'http://test/graphql',
+          }),
+        }
+      },
+      props: {
+        id: '1'
+      }
+    });
+
+    await waitForExpect(() => {
+      expect(wrapper.get('[data-test-id="form-title"').text()).toMatch('Editing Windows');
+    });
+  });
+});
